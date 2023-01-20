d2395362dadf5f18b5ddb4e05b07acc5c07f9b5a
Cícero Henrique <cicerohenrique282@gmail.com>
2023-01-20 09:04:35 -0300

    add tipos

---
+++ sintaxe-java/src/TipoDados.java
@@ -0,0 +1,10 @@
+public class TipoDados {
+    public static void main(String[] args) {
+        byte idade = 22;
+        short ano = 2021;
+        int cep = 65630526; // Se começar com zero, provavelmente deve ser outro tipo (String)
+        long cpf = 99988877766L; // Se começar com zero, provavelmente deve ser outro tipo (String)
+        float pi = 3.14F;
+        double salario = 1275.33;
+    }
+}
---
+++ sintaxe-java/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+class TiposVariaveis {
+    public static void main(String[] args) throws Exception{
+        int idade;
+        int anoFabricacao = 2021;
+        double salarioMinimo = 2500;
+
+        //Casting
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //Constantes
+        // Em outras linguagens se utiliza a palavra reservada const, em Java se utiliza final
+
+    }
+}
